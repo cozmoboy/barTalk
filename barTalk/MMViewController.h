a28c41328982f485a5027e4c187a275247e23f90
cozmoboy <cozmoboy@yahoo.com>
2013-02-06 14:26:41 -0600

    Initial Commit

---
+++ barTalk/MMViewController.h
@@ -0,0 +1,13 @@
+//
+//  MMViewController.h
+//  barTalk
+//
+//  Created by David Johnston on 2/6/13.
+//  Copyright (c) 2013 David Johnston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMViewController : UIViewController
+
+@end
